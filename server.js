fce8bca9d24a43956f5115a22bc46158049a036c
Kangue Loic <kangue.patrick21@myiuc.com>
2024-03-18 15:48:21 +0100

    Module user complet

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./database/db');
+const {readdirSync} = require('fs')
+
+const app  = express()
+
+//Utilisation du fichier .env pour la recuperation de certaine données
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+//Middlewares
+app.use(express.json())
+app.use(cors())
+
+//Fixation du suffixe "/api/v1/ à nos routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () => {
+   db()
+   app.listen(PORT, () => {
+    console.log('You are listening to port : ', PORT);
+   })
+}
+
+server();
