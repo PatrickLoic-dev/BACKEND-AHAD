0ea7a754b9de876accf692445d92d157b3fdada4
Loïc Patrick <106379951+PatrickLoic-dev@users.noreply.github.com>
2024-04-09 17:38:37 +0100

    Update constantes.js

--- frontend/utils/constantes.js
+++ frontend/utils/constantes.js
@@ -1,1 +1,6 @@
-export const principalColor = 'white';
+export const principalColor = '#000';
+export const textSecondaryColor = '#575757';
+export const textPrimaryColor = '#000';
+export const SecondaryColor = '#BFBFBF';
+export const red = '#FF0000';
+export const OnBoardingColor = '#F1F1F1';
