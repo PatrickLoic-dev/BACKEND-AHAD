0509c910a2380df635e16b1c931a7d0d57f2adcb
Kangue Loic <kangueloic9@gmail.com>
2024-05-02 22:25:52 +0100

    Ajout de l'nevoie de mail

--- middlewares/notification.js
+++ middlewares/notification.js
@@ -13,4 +13,24 @@ const transporter = nodemailer.createTransport({
     }
 })
 
-let mai
+let mailOptions = {
+    from : {
+        name : "AHAD",
+        address : process.env.EMAIL_USER
+    },
+    to: 'kangueloic9@gmail.com',
+    subject : "Welcome to ahad",
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+}
+
+const sendMail = async (transporter, mailOptions) => {
+    try {
+        await transporter.sendMail(mailOptions)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+sendMail(transporter, mailOptions);
--- server.js
+++ server.js
@@ -3,6 +3,41 @@ const cors = require('cors');
 const { db } = require('./database/db');
 const {readdirSync} = require('fs');
 const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+   service : 'gmail',
+   host : "smtp.gmail.com",
+   port : 587,
+   secure : false,
+   auth : {
+      user: process.env.EMAIL_USER, 
+      pass : process.env.EMAIL_PASS,
+   }
+})
+
+let mailOptions = {
+   from : {
+      name : "AHAD",
+      address : process.env.EMAIL_USER
+   },
+   to: 'kangueloic9@gmail.com',
+   subject : "Welcome to ahad",
+   subject: "Hello ✔", // Subject line
+   text: "Hello world?", // plain text body
+   html: "<b>Hello world?</b>", // html body
+}
+
+const sendMail = async (transporter, mailOptions) => {
+   try {
+      await transporter.sendMail(mailOptions )
+      console.log('Email has been sent')
+   } catch (error) {
+      console.error(error)
+   }
+}
 
 const app  = express()
 
@@ -20,6 +55,7 @@ app.use('/uploads', express.static('uploads'))
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
+sendMail(transporter, mailOptions);
    db();
    app.listen(PORT, () => {
       console.log('You are listening to port : ', PORT);
